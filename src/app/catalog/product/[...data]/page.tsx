b506f473b32ae690d3438e6f040517d290af7150
Gabriel Maia <gribeiromaia736@gmail.com>
2023-11-21 18:56:29 -0300

    learn about data fetch in components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 interface ProductProps {
   params: {
     data: string[]
@@ -7,11 +8,17 @@ interface ProductProps {
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
 
+  function addCart() {
+    console.log('Adicionado')
+  }
+
   return (
     <div>
       <h1>Product: {productId}</h1>
       <h1>Tamanho: {size}</h1>
       <h1>Cor: {color}</h1>
+
+      <button onClick={addCart}>Adicionar ao Carrinho</button>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Hello World!!</h1>
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('https://api.github.com/users/GabriellMaiaa')
+  const user = await response.json()
+
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+      <p>{JSON.stringify(user, null, 2)}</p>
+    </div>
+  )
 }
